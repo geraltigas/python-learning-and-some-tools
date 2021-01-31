ceb9b1a8b38336bb762e87e415aac9ac121384d6
geraltigas <jb030112@outlook.com>
2021-01-31 20:14:17 +0800

    '便携式输入自然推理公式'

---
+++ Natural Deduction Transfer.py
@@ -0,0 +1,13 @@
+import pyperclip
+while True:
+    a = input("typein the string to be format:")
+    a = a.replace("js","Γ")
+    a = a.replace("dl","┠")
+    a = a.replace("ji","∧")
+    a = a.replace("bi","∨")
+    a = a.replace("yh","→")
+    a = a.replace("dj","↔")
+    a = a.replace("cz","∃")
+    a = a.replace("sy","∀")
+    pyperclip.copy(a)
+    i = input("")
