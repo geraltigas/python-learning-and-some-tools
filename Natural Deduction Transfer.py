ac0227ad0667a5fa6e750e69d2ad468a7868f91b
geraltigas <69190887+geraltigas@users.noreply.github.com>
2021-02-14 18:42:34 +0800

    Update Natural Deduction Transfer.py

--- Natural Deduction Transfer.py
+++ Natural Deduction Transfer.py
@@ -1,14 +1,5 @@
 import pyperclip
 while True:
-    a = input("typein the string to be format:")
-    a = a.replace("js","Γ")
-    a = a.replace("dl","┠")
-    a = a.replace("ji","∧")
-    a = a.replace("bi","∨")
-    a = a.replace("yh","→")
-    a = a.replace("dj","↔")
-    a = a.replace("cz","∃")
-    a = a.replace("sy","∀")
-    a = a.replace("fe","┓")
+    a = input("typein the string to be format:").replace("js","Γ").replace("dl","┠").replace("ji","∧").replace("bi","∨").replace("yh","→").replace("dj","↔").replace("cz","∃").replace("sy","∀").replace("fe","┓")
     pyperclip.copy(a)
     i = input("")
